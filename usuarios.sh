d6cfa756e4286216a9df21ea32372e0c4e472c82
nachorivas581 <64506488+nachorivas581@users.noreply.github.com>
2020-10-23 20:31:33 +0200

    Create usuarios.sh

---
+++ usuarios.sh
@@ -0,0 +1,11 @@
+echo -e "\e[7mElije el usuario?\e[0m"
+           select yn in "abono" "otro" "prepago" ; do
+             case $yn in
+                   abono) adduser abono;break ;;
+                   otro) adduser otro ;;
+                   prepago) adduser prepago ;;
+                   esac
+                   done
+sleep 3; echo -e "\e[7mCorrecto!\e[0m"
+
+
